3a541ee725161692c2a64ec02d7bf6a3a8f2fe54
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-02-19 20:26:36 -0500

    Added basic python package setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Geographic Coordinate Converter',
+    url='https://github.com/boikle/geographic_coordinate_converter',
+    author='Robert Oikle',
+    packages=['geographic_coordinate_converter'],
+    install_requires=[],
+    version='0.1',
+    license='MIT',
+    description='A simple geographic coordinate converter',
+    long_description=open('README.md').read()
+)
