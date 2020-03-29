2069f702b15d21e0f5b4b9803a9bae0d059e64fc
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-03-28 21:30:33 -0400

    Updated ui layout

--- converter/main.py
+++ converter/main.py
@@ -1,28 +1,29 @@
 from tkinter import Tk, Label, Entry, Button
+import tkinter.scrolledtext as tkscroll
 
 class Converter:
     '''
     Class: Converter
     Description: UI for converting geographic coordinates into various formats
     '''
-    def __init__(self, master):
-        self.master = master
-        master.title("Geographic Coordinate Converter")
+    def __init__(self, window):
+        self.window = window
+        window.resizable(width=False, height=False)
+        window.geometry('480x220')
+        window.title("Geographic Coordinate Converter")
 
-        self.lat_label = Label(master, text="Latitude")
-        self.lat_label.pack()
+        self.lat_label = Label(window, text="Latitude").grid(row=0, pady=2)
+        self.long_label = Label(window, text="Longitude").grid(row=1, pady=2)
 
-        self.lat = Entry(master)
-        self.lat.pack()
+        self.lat = Entry(window, width=50)
+        self.long = Entry(window, width=50)
 
-        self.long_label = Label(master, text="Longitude")
-        self.long_label.pack()
+        self.lat.grid(row=0, column=1)
+        self.long.grid(row=1, column=1)
 
-        self.long = Entry(master)
-        self.long.pack()
+        self.convert_button = Button(window, text="Convert", command=self.convert).grid(row=2, columnspan=2, pady=5)
 
-        self.convert_button = Button(master, text="Convert", command=self.convert)
-        self.convert_button.pack()
+        self.result = tkscroll.ScrolledText(window, width=55, height=6).grid(row=3, columnspan=2, padx=2, pady=2)
 
     def convert(self):
         '''Future convert function code'''
